0b743e2b09a3da0812147f83c449cf9e10a59dad
Kevin Druff <kevin.druff@gmail.com>
2022-04-09 12:00:11 -0400

    added code; need to add tests
    temp

--- kmd-saj/apps/saj-demo-e2e/src/integration/app.spec.ts
+++ kmd-saj/apps/saj-demo-e2e/src/integration/app.spec.ts
@@ -1,7 +1,76 @@
 import { getGreeting } from '../support/app.po';
+import { faker } from '@faker-js/faker';
+import { ApplicantSummaryApiResponse } from '@kmd-saj/summary-results';
+
+function defaultData(): ApplicantSummaryApiResponse[] {
+  const fakeData = [];
+  for (let i = 0; i < 20; i++) {
+    const answer =
+      faker.random.number({
+        min: 0,
+        max: 1,
+      }) === 0
+        ? `yes`
+        : `no`;
+
+    const fakeUser = {
+      id: i,
+      name: `${faker.name.firstName()} ${faker.name.lastName()}`,
+      position: `${faker.name.jobTitle()}`,
+      applied: `${faker.date.past()}`,
+      experience: faker.datatype.number({
+        min: 0,
+        max: 20,
+        precision: 2,
+      }),
+      availability: {
+        M: faker.datatype.number({
+          min: 0,
+          max: 2,
+        }),
+        T: faker.datatype.number({
+          min: 0,
+          max: 2,
+        }),
+        W: faker.datatype.number({
+          min: 0,
+          max: 2,
+        }),
+        Th: faker.datatype.number({
+          min: 0,
+          max: 2,
+        }),
+        F: faker.datatype.number({
+          min: 0,
+          max: 2,
+        }),
+        S: faker.datatype.number({
+          min: 0,
+          max: 2,
+        }),
+        Su: faker.datatype.number({
+          min: 0,
+          max: 2,
+        }),
+      },
+      questions: [
+        {
+          text: faker.lorem.sentence(),
+          answer: answer,
+        },
+      ],
+    };
+    fakeData.push(fakeUser);
+  }
+  console.log(`fakeData`, fakeData);
+  return fakeData;
+}
 
 describe('saj-demo', () => {
-  beforeEach(() => cy.visit('/'));
+  beforeEach(() => {
+    cy.visit('/');
+    cy.intercept(`/summary-results`, defaultData());
+  });
 
   it('should display welcome message', () => {
     // Custom command example, see `../support/commands.ts` file
--- kmd-saj/apps/saj-demo/src/app/app.module.ts
+++ kmd-saj/apps/saj-demo/src/app/app.module.ts
@@ -1,12 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+import { SummaryResultsModule } from '@kmd-saj/summary-results';
 
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
+
+const routes: Routes = [
+  {
+    path: ``,
+    redirectTo: `applicant-summary`,
+    pathMatch: `full`,
+  },
+
+  {
+    path: `applicant-summary`,
+    loadChildren: () =>
+      import(`@kmd-saj/summary-results`).then((m) => m.SummaryResultsModule),
+    //todo: canActivate
+  },
+  {
+    path: `applicant`,
+    loadChildren: () =>
+      import(`@kmd-saj/applicant-details`).then(
+        (m) => m.ApplicantDetailsModule
+      ),
+    //todo: canActivate
+  },
+];
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, SummaryResultsModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent],
 })
